7a7c17c46999f5726f1e258126665b78ee9bf25d
Gonzalo <gonzalo.godet@gmail.com>
2022-12-01 00:14:03 -0600

    added driver home page

---
+++ lib/driver_home_page/driver_home_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rides_iteso/bloc/auth/auth_bloc.dart';
+import 'package:rides_iteso/bloc/user/user_bloc.dart';
+import 'package:rides_iteso/components/base_ElevatedButton.dart';
+import 'package:rides_iteso/driver_home_page/driver_card.dart';
+import 'package:rides_iteso/login/login_page.dart';
+import 'package:rides_iteso/rides/driver/driver_rides_page.dart';
+import 'package:rides_iteso/rides/pass_driv_button.dart';
+import 'package:rides_iteso/rides/passenger/passenger_ride_page.dart';
+
+import '../bloc/auth/auth.dart';
+
+class DriverHomePage extends StatefulWidget {
+  const DriverHomePage({super.key});
+
+  @override
+  State<DriverHomePage> createState() => _DriverHomePageState();
+}
+
+class _DriverHomePageState extends State<DriverHomePage> {
+  bool isDriver = true;
+  String userUid = '';
+
+  @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<UserBloc>(context).state;
+    print('UserBloc: ${BlocProvider.of<UserBloc>(context).state}');
+  }
+
+  Future<void> signOut() async {
+    BlocProvider.of<AuthBloc>(context).add(SignOutRequested());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.black,
+        elevation: 0.0,
+        actions: [
+          BlocConsumer<AuthBloc, AuthState>(
+            listener: (context, state) {
+              if (state is UnAuthenticated) {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (context) => const LoginPage(),
+                  ),
+                );
+              }
+            },
+            builder: (context, state) {
+              return IconButton(
+                  onPressed: () => signOut(),
+                  icon: const Icon(Icons.logout_outlined));
+            },
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                child: Center(
+                  child: Column(
+                    children: [
+                      const Center(
+                        child: Text(
+                          'Pickup',
+                          style: TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const DriverCard(
+                        ride: {
+                          "name": "Juan Perez torrez ",
+                          "location": 'Plaza del Sol',
+                          "passengerLimit": 4,
+                          "time": "16:00",
+                        },
+                      ),
+                      base_ElevatedButton(
+                        text: "Añadir dias",
+                        backgroundColor: Colors.white,
+                        textColor: const Color(0xFF064789),
+                        onPressed: () {
+                          addDates();
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  addDates() {
+    print("add dates");
+  }
+
+  guardarButton(BuildContext context) {
+    /*   Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SignupPage()),
+    ); */
+  }
+
+  handleViewChange(context) {
+    setState(() {
+      isDriver = !isDriver;
+    });
+  }
+}
