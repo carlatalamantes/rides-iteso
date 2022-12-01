d527173663d436f2bda0fd51ebb7b258075b641a
ctalamanteskinectify <ctalamantes@kinectify.com>
2022-12-01 01:49:00 -0600

    Adding bloc data

--- lib/driver_home_page/driver_home_page.dart
+++ lib/driver_home_page/driver_home_page.dart
@@ -35,139 +35,82 @@ class _DriverHomePageState extends State<DriverHomePage> {
     BlocProvider.of<AuthBloc>(context).add(SignOutRequested());
   }
 
-  var list = [
-    {
-      "registererd": 1,
-      "passengerLimit": 4,
-      "time": "12:00",
-      "from": "ITESO",
-      "to": "Plaza del Sol"
-    },
-    {
-      "registererd": 1,
-      "passengerLimit": 4,
-      "time": "16:00",
-      "from": "ITESO",
-      "to": "Plaza del Sol"
-    },
-    {
-      "registererd": 1,
-      "passengerLimit": 4,
-      "time": "23:00",
-      "from": "ITESO",
-      "to": "Plaza del Sol"
-    },
-    {
-      "registererd": 1,
-      "passengerLimit": 4,
-      "time": "1:00",
-      "from": "ITESO",
-      "to": "Plaza del Sol"
-    },
-    {
-      "registererd": 1,
-      "passengerLimit": 4,
-      "time": "4:00",
-      "from": "ITESO",
-      "to": "Plaza del Sol"
-    },
-    {
-      "registererd": 1,
-      "passengerLimit": 4,
-      "time": "23:00",
-      "from": "ITESO",
-      "to": "Plaza del Sol"
-    },
-    {
-      "registererd": 1,
-      "passengerLimit": 4,
-      "time": "7:00",
-      "from": "ITESO",
-      "to": "Plaza del Sol"
-    },
-    {
-      "registererd": 1,
-      "passengerLimit": 4,
-      "time": "9:00",
-      "from": "ITESO",
-      "to": "Plaza del Sol"
-    },
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        foregroundColor: Colors.black,
-        elevation: 0.0,
-        actions: [
-          BlocConsumer<AuthBloc, AuthState>(
-            listener: (context, state) {
-              if (state is UnAuthenticated) {
-                Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(
-                    builder: (context) => const LoginPage(),
-                  ),
-                );
-              }
-            },
-            builder: (context, state) {
-              return IconButton(
-                  onPressed: () => signOut(),
-                  icon: const Icon(Icons.logout_outlined));
-            },
-          )
-        ],
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Center(
-              child: Column(
-                children: [
-                  const Center(
-                    child: Text(
-                      'Pickup',
-                      style: TextStyle(fontSize: 16, color: Colors.grey),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          foregroundColor: Colors.black,
+          elevation: 0.0,
+          actions: [
+            BlocConsumer<AuthBloc, AuthState>(
+              listener: (context, state) {
+                if (state is UnAuthenticated) {
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const LoginPage(),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: list.length,
-                    itemBuilder: (context, index) {
-                      return DriverCard(
-                        ride: list[index],
-                        index: index
-                      );
-                    },
-                  ),
-                  base_ElevatedButton(
-                    text: "Añadir dias",
-                    backgroundColor: const Color(0xFF064789),
-                    onPressed: () {
-                      addDates();
-                    },
-                  ),
-                  base_ElevatedButton(
-                    text: "Future Days",
-                    backgroundColor: const Color(0xFF064789),
-                    onPressed: () {
-                      addDates();
-                    },
-                  )
-                ],
-              ),
-            ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                return IconButton(
+                    onPressed: () => signOut(),
+                    icon: const Icon(Icons.logout_outlined));
+              },
+            )
           ],
         ),
-      ),
-    );
+        body: BlocConsumer<RoutesBloc, RoutesState>(
+          listener: (context, state) {},
+          builder: (context, state) {
+            return SingleChildScrollView(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Center(
+                    child: Column(
+                      children: [
+                        const Center(
+                          child: Text(
+                            'Pickup',
+                            style: TextStyle(fontSize: 16, color: Colors.grey),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        ListView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: state.routes.length,
+                          itemBuilder: (context, index) {
+                            return DriverCard(
+                                ride: state.routes[index], index: index);
+                          },
+                        ),
+                        base_ElevatedButton(
+                          text: "Añadir dias",
+                          backgroundColor: const Color(0xFF064789),
+                          onPressed: () {
+                            addDates();
+                          },
+                        ),
+                        base_ElevatedButton(
+                          text: "Future Days",
+                          backgroundColor: const Color(0xFF064789),
+                          onPressed: () {
+                            addDates();
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ));
   }
 
   addDates() {
